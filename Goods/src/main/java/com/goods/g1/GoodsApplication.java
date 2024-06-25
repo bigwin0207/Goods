3d1876f77db8334a0affb256bedcfaccf084a7d6
DESKTOP-F4BC13L\hi <bigwin0207@naver.com>
2024-06-25 10:00:16 +0900

    폴더정리

---
+++ Goods/src/main/java/com/goods/g1/GoodsApplication.java
@@ -0,0 +1,13 @@
+package com.goods.g1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsApplication.class, args);
+    }
+
+}
---
+++ Goods/src/test/java/com/goods/g1/GoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goods.g1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
